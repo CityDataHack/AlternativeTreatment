6eddfec00c3e87b9f1bb9435a56ad9025a0c45ad
Ilja <iljapanic@gmail.com>
2018-03-17 15:13:25 +0000

    Node Express boilerplate

---
+++ index.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+
+// route handler
+app.get('/', (request, response) => {
+  response.render('home', {
+    name: 'City Data Hack 2018'
+  })
+})
