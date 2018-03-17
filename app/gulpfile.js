1411852d12ac1453c92d34e3afdd1d491916491d
Ilja <iljapanic@gmail.com>
2018-03-17 17:26:00 +0000

    add gulp with browsersync and support for running the node app

---
+++ app/gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
+
+gulp.task('default', ['browser-sync'], function () {});
+
+gulp.task('browser-sync', ['nodemon'], function() {
+  browserSync.init(null, {
+    proxy: "http://localhost:3000",
+    files: ["**/**/*.*"],
+    port: 7000
+  });
+});
+
+gulp.task('nodemon', function (cb) {
+
+  var started = false;
+
+  return nodemon({
+    script: 'app.js'
+  }).on('start', function () {
+    if (!started) {
+      cb();
+      started = true;
+    }
+  });
+});
