8fe692f57d41483a6304a3d51f047a2f0a4ea6c8
Maciej Ziarkowski <ziarrek@gmail.com>
2018-03-18 10:08:44 +0000

    Add simple events and board API

--- app/apiRouter.js
+++ app/apiRouter.js
@@ -6,16 +6,33 @@ router.get('/test', function(req, res){
     res.json({'test': 1, 'test2': 'test'})
 })
 
+var events = [
+    {name: "Choir concert", start_time: "2018-03-19T19:00", address: "St. Mark's Church Barking & Dagenham"},
+    {name: "Farmer's market", start_time: "2018-03-20T12:00", end_time: "2018-03-20T16:00", address: "Square in front of Town Hall"}
+]
+
+var board = [
+    {
+        text: "Everyone is invited to flat B for an apple pie Tuesday evening!"
+    }
+]
+
 router.get('/events', function(req, res){
 
-    res.json([
-        {name: "Choir concert", start_time: "2018-03-19T19:00", address: "St. Mark's Church Barking & Dagenham"},
-        {name: "Farmer's market", start_time: "2018-03-20T12:00", end_time: "2018-03-20T16:00", address: "Square in front of Town Hall"}
-    ])
+    res.json(events)
 })
 
 router.get('/board', function(req,res){
-    res.json([])
+    res.json(board)
+})
+
+router.post('/board', function(req, res){
+
+    var message = {
+        text: req.IDONTKNOW
+    }
+
+    board.push(message)
 })
 
 
